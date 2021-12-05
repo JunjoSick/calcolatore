aeaf6809633d2151740cc36cffa97c1dda21398d
Theramar <lorenzo1.cini@gmail.com>
2021-12-05 11:32:04 +0100

    firstGrade

--- risolutore_equazioni.cpp
+++ risolutore_equazioni.cpp
@@ -1,12 +1,29 @@
 #include <math.h>
 #include <array>
+#include <exception>
+
+enum solution : unsigned char {
+	IMPOSSIBLE=0,
+	INDETERMINATE
+};
+
 template <int size> class equation {
 private:
 
 	std::array<double, size> coefficient;
 
 	auto firstGrade() {
-
+		if (coefficient[0] == 0) {
+			if (coefficient[1] == 0) {
+				throw INDETERMINATE;
+			}
+			else {
+				throw IMPOSSIBLE;
+			}
+		}
+		else {
+			return -coefficient[1] / coefficient[a];
+		}
 	}
 
 	auto secondGrade() {
@@ -27,6 +44,7 @@ public:
 		case 3: thirdGrade();
 			break;
 		default:
+			throw std::exception("grado troppo alto");
 		}
 	}
 };
