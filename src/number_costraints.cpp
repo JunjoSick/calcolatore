06a04efa8df47b3ff7e13d4d49edb4ee560f02b3
Theramar <81759060+Theramar@users.noreply.github.com>
2022-02-01 18:30:01 +0100

    Rename number_costraints.cpp to numberCostraints.cpp

--- src/number_costraints.cpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include <concepts>
-
-template <typename t> concept natural = std::unsigned_integral<t>;
-//template <typename t> concept number = std::integral<t> || std::floating_point<t> || std::is_same<t, std::complex<double>>::value;
